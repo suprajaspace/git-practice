ed21a8a718ce4d8f46f5adbfd979e21c0d08cd7e
supraja <supra556699@gmail.com>
2024-08-25 19:00:20 +0800

    conversations

--- conversations.sh
+++ conversations.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-echo "Ramesh : Hi Suresh, How r U?"
-echo "suresh:: hello ramesh.Iam fine,how r h doing?"
+echo "Ramesh:: Hi Suresh, How r u?"
+echo "suresh:: hello Ramesh.Iam fine,how r h doing?"
+echo "Ramesh::I am doing good. suresh  what's going on?"
+echo "suresh::I started Learning shell script Ramesh"

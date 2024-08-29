21011d7747c4b1bd7d8abf5e74280a9a7fa44ad5
supraja <supra556699@gmail.com>
2024-08-29 23:17:12 +0800

    shell scripting

--- 16.practice.sh
+++ 16.practice.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "practice"
 NUMBER1=$1
 NUMBER2=$2
 SUM=$(($NUMBER1+$NUMBER2))
@@ -13,4 +14,11 @@ R="\e[31m"
 N="\e[0m"
 
 echo -e "$R supraja $N"
+
+if [ $1 -ge 20 ]
+then 
+echo " arguments passed"
+else
+echo "arguments not passed"
+fi
 

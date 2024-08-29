3e0daedd1f9aeeb72d830edf6fa50fda8abda87d
supraja <supra556699@gmail.com>
2024-08-29 21:56:24 +0800

    shell scripting

--- 16.redirectors.sh
+++ 16.redirectors.sh
@@ -35,7 +35,7 @@ USAGE(){
     echo -e "$R USAGE :: $N sudo sh 16.redirectors.sh package1 package2.."
     exit 1
 }
-    echo "script started executing at :$(date)" | tee -a $LOG_FILE
+echo "script started executing at : $(date)" | tee -a $LOG_FILE
 CHECK_ROOT
 if [ $# -eq 0 ]
 then 
